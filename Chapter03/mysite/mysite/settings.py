4582a85c09d52cb5cb6d6da4ace5f67b3a9fb712
aris <winandiaris@gmail.com>
2022-11-30 05:39:58 +0700

    blog complete run

--- Chapter03/mysite/mysite/settings.py
+++ Chapter03/mysite/mysite/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'blog',
-        'PASSWORD': 'xxxxxx',
+        'NAME': 'blog4',
+        'USER': 'aris',
+        'PASSWORD': 'aris1985',
     }
 }
 
